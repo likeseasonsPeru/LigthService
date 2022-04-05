3846dea253fd9a4a334c60489b4ba3fa5d3e481c
Carlito <rody.castillo.tacuri@gmail.com>
2022-04-05 13:10:48 -0500

    chore[calc.js]: add input per apartments

--- src/components/calc.js
+++ src/components/calc.js
@@ -3,6 +3,7 @@ import { AppContext } from "../context/AppContext";
 import "../index.css";
 
 export const Calc = () => {
+  const [depas, setDepas] = useState({});
   const [data, setData] = useState({
     kWh: null,
     pricekWh: null,
@@ -13,17 +14,28 @@ export const Calc = () => {
     totalMonth: null,
     totalPay: null,
     nroDepas: null,
+    show: false,
   });
   const { show, dispatch } = useContext(AppContext);
 
-  const saveData = () => {
+  const saveData = (e) => {
+    e.preventDefault();
+    for (const a in data) {
+      if (data[a] === null || data[a] === isNaN) {
+        return alert("complete the boxes pls!!");
+      }
+    }
     localStorage.setItem("dataform", JSON.stringify(data));
-    dispatch({ type: "SAVEFORM", payload: { dataform: data, show: false } });
+    setData({ ...data, show: true });
   };
   function round(num) {
     var m = Number((Math.abs(num) * 100).toPrecision(15));
     return (Math.round(m) / 100) * Math.sign(num);
   }
+  const nextStep = () => {
+    localStorage.setItem("dataform", JSON.stringify(data));
+    dispatch({ type: "SAVEFORM", payload: { dataform: data, show: false } });
+  };
 
   useEffect(() => {
     const localData = JSON.parse(localStorage.getItem("dataform"));
@@ -34,170 +46,195 @@ export const Calc = () => {
   }, []);
   return (
     <div>
-      {show && (
-        <form onSubmit={saveData}>
-          <h1 className="text-center text-black">RECIBO TOTAL</h1>
-
-          <div className="row text-center w-100 justify-content-center">
+      {show &&
+        (data.show ? (
+          <div className="row">
+            <code>{JSON.stringify(data)}</code>
             <div className="col-auto">
-              <label htmlFor="kilowats" className=" text-black py-2">
-                Consumo kWh:
-              </label>
-              <input
-                type="number"
-                className="form-control"
-                id="kilowats"
-                step="any"
-                value={data.kWh}
-                onChange={(e) =>
-                  setData({ ...data, kWh: parseFloat(e.target.value) })
-                }
-              />
-            </div>
-            <div className="col-auto">
-              <label htmlFor="price" className=" text-black py-2">
-                {" "}
-                {data.kWh ? data.kWh : "..."} kWh al precio de
-              </label>
-              <input
-                type="number"
-                className="form-control"
-                id="price"
-                step="any"
-                value={data.pricekWh}
-                onChange={(e) =>
-                  setData({ ...data, pricekWh: parseFloat(e.target.value) })
-                }
-              />
-            </div>
-            <div className="col-auto">
-              <label htmlFor="totalEnergy" className=" text-black py-2">
-                Cargo por Energía
-              </label>
-              <input
-                type="number"
-                className="form-control"
-                disabled
-                step="any"
-                id="totalEnergy"
-                value={
-                  (data.totalEnergy = round(
-                    parseFloat(data.pricekWh) * parseFloat(data.kWh)
-                  ))
-                }
-              />
+              {data.nroDepas &&
+                [...Array(data.nroDepas)].map((_, i) => (
+                  <>
+                    <label htmlFor="kilowats" className=" text-black py-2">
+                      Consumo kWh:
+                    </label>
+                    <input
+                      type="number"
+                      className="form-control"
+                      id="kilowats"
+                      step="any"
+                      value={data.kWh}
+                      onChange={(e) =>
+                        setData({ ...data, kWh: parseFloat(e.target.value) })
+                      }
+                    />
+                  </>
+                ))}
             </div>
           </div>
-          <div className="row text-center w-100 justify-content-center">
-            <div className="col-auto">
-              <label htmlFor="enel" className=" text-black py-2">
-                Gastos Enel
-              </label>
-              <input
-                type="number"
-                className="form-control"
-                id="enel"
-                step="any"
-                value={data.enel}
-                onChange={(e) =>
-                  setData({ ...data, enel: parseFloat(e.target.value) })
-                }
-              />
-            </div>
-            <div className="col-auto">
-              <label htmlFor="igv" className=" text-black py-2">
-                IGV
-              </label>
-              <input
-                type="number"
-                className="form-control"
-                disabled
-                id="igv"
-                step="any"
-                value={
-                  (data.igv = round(
-                    (parseFloat(data.totalEnergy) + parseFloat(data.enel)) *
-                      0.18
-                  ))
-                }
-              />
-            </div>
-            <div className="col-auto">
-              <label htmlFor="ley" className=" text-black py-2">
-                Total Mes Actual
-              </label>
-              <input
-                type="number"
-                className="form-control"
-                disabled
-                step="any"
-                id="ley"
-                value={
-                  (data.totalMonth = round(
-                    parseFloat(data.igv) +
-                      parseFloat(data.totalEnergy) +
-                      parseFloat(data.enel)
-                  ))
-                }
-              />
-            </div>
-            <div className="row justify-content-center align-items-end">
+        ) : (
+          <form onSubmit={saveData}>
+            <h1 className="text-center text-black">RECIBO TOTAL</h1>
+
+            <div className="row text-center w-100 justify-content-center">
               <div className="col-auto">
-                <label htmlFor="totalPay" className=" text-black py-2">
-                  Aporte Ley N 28749:
+                <label htmlFor="kilowats" className=" text-black py-2">
+                  Consumo kWh:
                 </label>
                 <input
                   type="number"
                   className="form-control"
-                  id="totalPay"
+                  id="kilowats"
                   step="any"
-                  value={data.ley}
-                  onChange={(e) => {
-                    setData({ ...data, ley: parseFloat(e.target.value) });
-                  }}
+                  value={data.kWh}
+                  onChange={(e) =>
+                    setData({ ...data, kWh: parseFloat(e.target.value) })
+                  }
                 />
               </div>
               <div className="col-auto">
-                <label htmlFor="totalPay" className=" text-black py-2">
-                  Total a Pagar:
+                <label htmlFor="price" className=" text-black py-2">
+                  {" "}
+                  {data.kWh ? data.kWh : "..."} kWh al precio de
+                </label>
+                <input
+                  type="number"
+                  className="form-control"
+                  id="price"
+                  step="any"
+                  value={data.pricekWh}
+                  onChange={(e) =>
+                    setData({ ...data, pricekWh: parseFloat(e.target.value) })
+                  }
+                />
+              </div>
+              <div className="col-auto">
+                <label htmlFor="totalEnergy" className=" text-black py-2">
+                  Cargo por Energía
                 </label>
                 <input
                   type="number"
                   className="form-control"
                   disabled
                   step="any"
-                  id="totalPay"
+                  id="totalEnergy"
                   value={
-                    (data.totalPay =
-                      parseFloat(data.totalMonth) + parseFloat(data.ley))
+                    (data.totalEnergy = round(
+                      parseFloat(data.pricekWh) * parseFloat(data.kWh)
+                    ))
                   }
                 />
               </div>
+            </div>
+            <div className="row text-center w-100 justify-content-center">
               <div className="col-auto">
-                <label htmlFor="nroDepas" className=" text-black py-2">
-                  Nro de Departamentos:
+                <label htmlFor="enel" className=" text-black py-2">
+                  Gastos Enel
                 </label>
                 <input
                   type="number"
                   className="form-control"
-                  id="nroDepas"
-                  value={data.nroDepas}
-                  onChange={(e) => {
-                    setData({ ...data, nroDepas: parseInt(e.target.value) });
-                  }}
+                  id="enel"
+                  step="any"
+                  value={data.enel}
+                  onChange={(e) =>
+                    setData({ ...data, enel: parseFloat(e.target.value) })
+                  }
                 />
               </div>
+              <div className="col-auto">
+                <label htmlFor="igv" className=" text-black py-2">
+                  IGV
+                </label>
+                <input
+                  type="number"
+                  className="form-control"
+                  disabled
+                  id="igv"
+                  step="any"
+                  value={
+                    (data.igv = round(
+                      (parseFloat(data.totalEnergy) + parseFloat(data.enel)) *
+                        0.18
+                    ))
+                  }
+                />
+              </div>
+              <div className="col-auto">
+                <label htmlFor="ley" className=" text-black py-2">
+                  Total Mes Actual
+                </label>
+                <input
+                  type="number"
+                  className="form-control"
+                  disabled
+                  step="any"
+                  id="ley"
+                  value={
+                    (data.totalMonth = round(
+                      parseFloat(data.igv) +
+                        parseFloat(data.totalEnergy) +
+                        parseFloat(data.enel)
+                    ))
+                  }
+                />
+              </div>
+              <div className="row justify-content-center align-items-end">
+                <div className="col-auto">
+                  <label htmlFor="totalPay" className=" text-black py-2">
+                    Aporte Ley N 28749:
+                  </label>
+                  <input
+                    type="number"
+                    className="form-control"
+                    id="totalPay"
+                    step="any"
+                    value={data.ley}
+                    onChange={(e) => {
+                      setData({ ...data, ley: parseFloat(e.target.value) });
+                    }}
+                  />
+                </div>
+                <div className="col-auto">
+                  <label htmlFor="totalPay" className=" text-black py-2">
+                    Total a Pagar:
+                  </label>
+                  <input
+                    type="number"
+                    className="form-control"
+                    disabled
+                    step="any"
+                    id="totalPay"
+                    value={
+                      (data.totalPay =
+                        parseFloat(data.totalMonth) + parseFloat(data.ley))
+                    }
+                  />
+                </div>
+                <div className="col-auto">
+                  <label htmlFor="nroDepas" className=" text-black py-2">
+                    Nro de Departamentos:
+                  </label>
+                  <input
+                    type="number"
+                    className="form-control"
+                    id="nroDepas"
+                    value={data.nroDepas}
+                    onChange={(e) => {
+                      setData({ ...data, nroDepas: parseInt(e.target.value) });
+                    }}
+                  />
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="row justify-content-center">
-            <div className="col-auto text-center py-3">
-              <button type="submit" className="btn btn-primary ">
-                Guardar
-              </button>
+            <div className="row justify-content-center">
+              <div className="col-auto text-center py-3">
+                <button type="submit" className="btn btn-primary ">
+                  Siguiente
+                </button>
+              </div>
             </div>
-          </div>
-        </form>
-      )}
+          </form>
+        ))}
     </div>
   );
 };
